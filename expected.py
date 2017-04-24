ffb42ff8df4bfe02903493234d99e3871ca4ae6a
Vivek Kumar Singh <vschs007@gmail.com>
2017-04-24 23:23:34 +0530

    indentation updated
    indentation updated

--- expected.py
+++ expected.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 class window_with_title(object):
     """An expectation for checking that a window exists with the specified title.
 
